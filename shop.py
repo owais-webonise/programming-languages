32550ba897922cc6f45f64c674009117ed7b9e68
owais-webonise <owais.kazi@weboniselab.com>
2017-11-06 14:32:57 +0530

    Implement Second Use Case

--- shop.py
+++ shop.py
@@ -1,6 +1,7 @@
 while(True):	
-	print "1. 1 Colgate"
-
+	print "1. Buy 1 Colgate"
+	print "2. Buy 10 Colgate"
+	
 	print "Enter your choice"
 	ch=input()
 
@@ -10,9 +11,17 @@ while(True):
 	def colgate_1():
 		colgate=1*10
 		bill_amount(colgate)
+	
+	def colgate_10():
+		colgate=10*10
+		bill_amount(colgate)
 
 	if ch==1:
 		colgate_1()
+
+	if ch==2:
+		colgate_10()
+
 
 
 
