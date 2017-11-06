51749f158f9ce3ff0b4d3d647732c9d146c026c5
owais-webonise <owais.kazi@weboniselab.com>
2017-11-06 14:40:38 +0530

    Implement Fourth Use Case

--- shop.py
+++ shop.py
@@ -2,6 +2,7 @@ while(True):
 	print "1. Buy 1 Colgate"
 	print "2. Buy 10 Colgates"
 	print "3. Buy 5 Colgates and 2 CloseUp"	
+	print "4. Colgate Offer(Buy 2 Get 1 Free)"
 	
 	print "Enter your choice"
 	ch=input()
@@ -22,6 +23,13 @@ while(True):
 		closeup=15*2
 		bill_amount(colgate + closeup)
 
+	def colgate_offer():
+		print "Enter number of Colgates"
+		co=input()
+		total=(co/3)*20 + (co%3)*10
+		bill_amount(total)
+	
+
 	if ch==1:
 		colgate_1()
 
@@ -30,6 +38,9 @@ while(True):
 	
 	if ch==3:
 		colgate_closeup()
+	
+	if ch==4:
+		colgate_offer()
 
 
 
