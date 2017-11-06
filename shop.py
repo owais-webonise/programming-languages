d8e50c8f08c5e0262f52c84df1f818118e552328
owais-webonise <owais.kazi@weboniselab.com>
2017-11-06 14:35:34 +0530

    Implement Third Use Case

--- shop.py
+++ shop.py
@@ -1,6 +1,7 @@
 while(True):	
 	print "1. Buy 1 Colgate"
-	print "2. Buy 10 Colgate"
+	print "2. Buy 10 Colgates"
+	print "3. Buy 5 Colgates and 2 CloseUp"	
 	
 	print "Enter your choice"
 	ch=input()
@@ -16,12 +17,19 @@ while(True):
 		colgate=10*10
 		bill_amount(colgate)
 
+	def colgate_closeup():
+		colgate=10*5
+		closeup=15*2
+		bill_amount(colgate + closeup)
+
 	if ch==1:
 		colgate_1()
 
 	if ch==2:
 		colgate_10()
-
+	
+	if ch==3:
+		colgate_closeup()
 
 
 
