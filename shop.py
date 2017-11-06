16f500769e0e3ff29d9d5a6cde6c9540d4b1f9be
owais-webonise <owais.kazi@weboniselab.com>
2017-11-06 14:29:20 +0530

    Implement First Use Case

---
+++ shop.py
@@ -0,0 +1,19 @@
+while(True):	
+	print "1. 1 Colgate"
+
+	print "Enter your choice"
+	ch=input()
+
+	def bill_amount(total):
+		print "Total amount is:-",total
+
+	def colgate_1():
+		colgate=1*10
+		bill_amount(colgate)
+
+	if ch==1:
+		colgate_1()
+
+
+
+
